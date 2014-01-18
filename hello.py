a01a75503b241d8fb88f1908fe26619277f076ce
Leah Alpert <leah@khanacademy.org>
2014-01-18 12:54:26 -0800

    added contact and about

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash
 from match_names import Matcher
 
 app = Flask(__name__)
@@ -23,8 +23,14 @@ def matches(name):
 def about():
     return render_template('about.html')
 
-@app.route('/contact/')
+@app.route('/contact/', methods=['POST', 'GET'])
 def contact():
+    if request.method == 'POST':
+        app.logger.info("You've got mail!")
+        app.logger.info(request.form['name'])
+        app.logger.info(request.form['email'])
+        app.logger.info(request.form['message'])
+        
     return render_template('contact.html')
 
 
